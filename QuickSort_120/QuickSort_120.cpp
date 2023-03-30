a2a97b9ce7fa864ebbc38db7ad03caba56f5373f
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-03-30 15:06:50 +1200

    int arr

--- QuickSort_120/QuickSort_120.cpp
+++ QuickSort_120/QuickSort_120.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];				// array of integrs to hold velues
-int cmp_cout = 0			// 
+int arr[20];				// array of integers to hold velues
+int cmp_count = 0;			// number of comparasion 
+int mov_count = 0;			// number of data movements
+int n;						
 
