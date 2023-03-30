2eb94bca7ebcc39a778e973b3ea8b92dee7d1177
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-03-30 15:52:48 +1200

    fungsi while, fungsi if

--- QuickSort_120/QuickSort_120.cpp
+++ QuickSort_120/QuickSort_120.cpp
@@ -41,4 +41,62 @@ void swap(int x, int y)
 	arr[y] = temp;
 }
 
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)			// Langkah algoritma no. 1
+		return;
+
+	//Partition the list into two parts:
+	//One containing elements less that or equal to pivot
+	//Outher conntainning elements greather than pivot
+
+	pivot = arr[low];				// Langkah algoritma no.2
+
+	i = low + 1;					// Langkah algoritma no.3
+	j = high;						// Langkah algoritma no.4
+
+	while (i <= j)					// Langkah algoritma no.10
+	{
+		//search for an element greather than pivot
+		while ((arr[i] <= pivot) && (i <= high))		// Langkah algoritma no. 5
+		{
+			i++;										// Langkah algoritma no. 6
+			cmp_count++;
+		}
+		cmp_count++;
+
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))			// Langkah algoritma no.7
+		{
+			j--;										// Langkah algoritma no.8
+			cmp_count++;
+		}
+		cmp_count++;
+
+		//if the greather element is on the left of the element
+		if (i < j)										// Langkah algoritma no.9
+		{
+			//swap the element at index i with the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	//j now containt the index of the last element in the sorted list
+	if (low < j)										// Langkah algoritma no.11
+	{
+		//move the pivot to its correct position in the list
+		swap(low, j);
+		mov_count++;
+	}
+	//sort the list on the left of pivot using quick sort
+	q_short(low, j - 1);								// Langkah algoritma no.12
+
+	//sort  the list on the right of pivot using quick sort
+	q_short(j + 1, high);								// Langkah algoritma no.13
+
+}
+
+
+
 
